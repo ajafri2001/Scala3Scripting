d95b0349b63253fd6d518d6ca50c325029b5030c
noga <nabarun.mondal@gmail.com>
2025-05-04 21:12:07 +0530

    Updated binding.

--- lib/src/test/java/org/zoomba/scala3/CodeGenTest.java
+++ lib/src/test/java/org/zoomba/scala3/CodeGenTest.java
@@ -4,12 +4,23 @@ import org.junit.Test;
 
 import javax.script.SimpleBindings;
 
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
 public class CodeGenTest {
 
     final String PRINT_HELLO = "println( \"Hello, World!\")" ;
 
+    final String USE_BINDING = "_mem.get( \"x\" ) " ;
+
     @Test
     public void helloWorld() throws Throwable{
         CodeGen.execute( PRINT_HELLO,   new SimpleBindings() );
+    }
+
+    @Test
+    public void useBinding() throws Throwable{
+        assertEquals( 42, CodeGen.execute( USE_BINDING,   new SimpleBindings(Map.of("x", 42 )) ) );
     }
 }
